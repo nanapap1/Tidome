99ccf5892cf520116b5f24072ef38b36852c8f0b
nanapap1 <markmoskvitin@MBP-Mark.Dlink>
2025-12-14 13:48:03 +0300

    Initial commit

---
+++ src/main/java/ru/MarkMoskvitin/Timedome/TimedomeApplication.java
@@ -0,0 +1,13 @@
+package ru.MarkMoskvitin.Timedome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimedomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimedomeApplication.class, args);
+	}
+
+}
